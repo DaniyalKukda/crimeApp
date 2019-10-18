7aaf06786d1400b0b16a3ddc25eb48e3997b205e
daniyalkukda <daniyalkukda545@gmail.com>
2019-10-17 23:59:31 -0700

    my robbed and All robbed work

--- src/Screens/Myrobbed.js
+++ src/Screens/Myrobbed.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import { Title, Logouticon } from "../../Components/headerOptions/options"
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { Content, Card, CardItem, Left, Body, Right } from 'native-base';
+import { Title, Logouticon } from "../../Components/headerOptions/options";
+import { firebase } from "../Config/firebase"
+import MapView, {
+  Marker
+} from 'react-native-maps';
+
+const screen = Dimensions.get('window');
+
+const ASPECT_RATIO = screen.width / screen.height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 class Myrobbed extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
       headerTitle: <Title navigation={navigation} />,
       headerRight: (
@@ -11,10 +22,83 @@ class Myrobbed extends React.Component {
       )
     }
   };
+  state = {
+    robbery: [],
+  }
+  fetchRobbery = () => {
+    var uid = firebase.auth().currentUser.uid;
+    let arr = []
+    firebase.firestore().collection("Robbed").doc(uid).collection("myRobbed").onSnapshot(vale => {
+      vale.forEach((doc) => {
+        let docid = doc.id;
+        let data = doc.data();
+        arr.push(data)
+      })
+      this.setState({
+        robbery: arr
+      })
+    })
+  }
+  componentDidMount() {
+    this.fetchRobbery()
+  }
   render() {
+    let { robbery } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Welcome to Myrobbed</Text>
+        <Text style={{ alignSelf:"center" ,marginTop: 20, fontSize: 25, fontWeight: "600" }} >My Robbed</Text>
+        <Content>
+          {
+            robbery.map((ele , i) => {
+              let date = new Date(ele.timeStamp)
+              let day = date.getDay();
+              let month = date.getMonth();
+              let year = date.getFullYear();
+              let hour = date.getHours();
+              let minut = date.getMinutes()
+              return <Card key={i} cardBody style={{ marginTop: 30 }}>
+                <CardItem>
+                  <MapView
+                    showsUserLocation={true}
+                    followsUserLocation={true}
+                    region={
+                      {
+                        latitude: ele.coords.latitude,
+                        longitude: ele.coords.longitude,
+                        latitudeDelta: LATITUDE_DELTA,
+                        longitudeDelta: LONGITUDE_DELTA,
+                      }
+                    }
+                    style={{
+                      width: 320, height: 200
+                    }}
+                  >
+                    <Marker
+                      ref={marker => {
+                        this.marker = marker;
+                      }}
+                      coordinate={{
+                        latitude: ele.coords.latitude,
+                        longitude: ele.coords.longitude
+                      }}
+                    />
+                  </MapView>
+                </CardItem>
+                <CardItem>
+                  <Left>
+                    <Text>{ele.deviceName}</Text>
+                  </Left>
+                  <Body>
+                    <Text>{ele.locationName}</Text>
+                  </Body>
+                  <Right>
+                    <Text>{year}-{month}-{day} {hour}:{minut}</Text>
+                  </Right>
+                </CardItem>
+              </Card>
+            })
+          }
+        </Content>
       </View>
     );
   }
@@ -24,8 +108,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
